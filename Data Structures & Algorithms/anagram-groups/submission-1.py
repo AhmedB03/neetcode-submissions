5619dd9ecb71a30200a356f28adb429f5c2faa6a
AhmedB03 <jeffbootaan21@gmail.com>
2026-06-20 18:16:56 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+
+        for st in strs:
+            re = {}
+            for s in st:
+                re[s] = 1 + re.get(s,0)
+            if frozenset(re.items()) in res:
+                res[frozenset(re.items())].append(st)
+            else:
+                
+                res[frozenset(re.items())] = [st]
+        
+        return list(res.values())
+            
