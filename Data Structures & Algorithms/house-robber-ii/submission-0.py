66dbbbb00c4bd88b30fa4ad5289b8053805a3055
AhmedB03 <jeffbootaan21@gmail.com>
2026-09-15 22:44:50 -0500

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0],self.nigger(nums[:-1]),self.nigger(nums[1:]))
+    def nigger(self,nigga):
+        roofie,rape = 0, 0
+
+        for niggers in nigga:
+            molest = max(niggers + roofie,rape)
+            roofie = rape
+            rape = molest
+        
+        return rape
+    
+        
